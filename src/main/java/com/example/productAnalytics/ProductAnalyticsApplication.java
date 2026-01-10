b1e65704077a4983198ab96cd16b3d9b2f0c85dd
Ash <ashutoshv191@gmail.com>
2026-01-10 09:10:30 +0530

    Init spring boot project

---
+++ src/main/java/com/example/productAnalytics/ProductAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.productAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAnalyticsApplication.class, args);
+	}
+
+}
